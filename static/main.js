09054083ed88b4aac11e6a6e0e7dd3615aa88d71
James Pieper <james.w.pieper@gmail.com>
2016-08-22 08:03:24 -0500

    Add resize button

--- static/main.js
+++ static/main.js
@@ -2,7 +2,7 @@
   var drawLine = false
 
   var canvas = document.getElementById('canvas')
-  var finalPos = {x: 0, y: 0}
+  var endPos = {x: 0, y: 0}
   var startPos = {x: 0, y: 0}
   var ctx = canvas.getContext('2d')
   canvas.width = 193
@@ -12,9 +12,12 @@
 
   function line (cnvs) {
     cnvs.beginPath()
-    // The +2 in this function is to fix a bug where the line starts before where the mose is positioned
+    // The +2 in this function is to fix a bug where
+    // the line starts before where the mose is positioned.
+    // Depending on the zoom and the size of the picture
+    // this may need to bo adujusted
     cnvs.moveTo(startPos.x + 2, startPos.y)
-    cnvs.lineTo(finalPos.x, finalPos.y)
+    cnvs.lineTo(endPos.x, endPos.y)
     cnvs.stroke()
   }
 
@@ -30,9 +33,9 @@
     }
   }
 
-  function drawOnFinalPos () {}
+  function drawOnEndPos () {}
   if (drawLine === true) {
-    finalPos = {x: (e.pageX - canvasOffset.left), y: e.pageY - canvasOffset.top}
+    endPos = {x: (e.pageX - canvasOffset.left), y: e.pageY - canvasOffset.top}
 
     clearCanvas()
     line(ctx)
@@ -40,7 +43,7 @@
 
   canvas.addEventListener('mousemove', function (e) {
     if (drawLine === true) {
-      finalPos = {x: (e.pageX - canvasOffset.left), y: e.pageY - canvasOffset.top}
+      endPos = {x: (e.pageX - canvasOffset.left), y: e.pageY - canvasOffset.top}
 
       clearCanvas()
       line(ctx)
@@ -59,14 +62,26 @@
   })
 
   window.addEventListener('mouseup', function (e) {
-    console.log(startPos, finalPos)
+    console.log(startPos, endPos)
     clearCanvas()
     // Replace with var that is second canvas
     line(ctx)
-    finalPos = {x: 0, y: 0}
+    endPos = {x: 0, y: 0}
     startPos = {x: 0, y: 0}
     drawLine = false
   })
 
-  ajax.get('/healthz', null, function(data) { console.log(data)});
+  document.getElementById('resize').addEventListener('click', function () {
+    var post = { 
+      line: {
+        start: startPos, 
+        end: endPos,
+      },
+      length: document.getElementById("length").value
+    }
+    console.log('Post: ', post)
+    ajax.post('/resize', JSON.stringify(post), function (data) {console.log(data)})
+  }, false)
+
+  ajax.get('/healthz', null, function (data) { console.log(data)})
 })()
